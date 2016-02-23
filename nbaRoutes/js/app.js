1d802a5b2640eea032ad58ad5a9ddfabc64230da
Kyle Barlow <kbazlo@yahoo.com>
2016-02-22 23:38:30 -0700

    finished up using routes to display multiple pages on a SPA

--- nbaRoutes/js/app.js
+++ nbaRoutes/js/app.js
@@ -4,6 +4,53 @@ app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
 
     $httpProvider.interceptors.push('httpRequestInterceptor');
 
-    // routing configuration code
+    // routing configuration code. $stateProvider object goes below
+    $stateProvider
+        .state("home", {
+            url: "/",
+            templateUrl: "js/home/homeTmpl.html",
+            controller: "homeCtrl",
+            views: {
+                "utahjazz": {
+                    templateUrl: "js/teams/teamTmpl.html",
+                    controller: "teamCtrl",
+                    resolve: { teamData:
+                        function(teamService) {
+                            return teamService.getTeamData("utahjazz");
+                        }
+                    }
+                },
+                "losangeleslakers": {
+                    templateUrl: "js/teams/teamTmpl.html",
+                    controller: "teamCtrl",
+                    resolve: { teamData:
+                        function(teamService) {
+                            return teamService.getTeamData("losangeleslakers");
+                        }
+                    }
+                },
+                "miamiheat": {
+                    templateUrl: "js/teams/teamTmpl.html",
+                    controller: "teamCtrl",
+                    resolve: { teamData:
+                        function(teamService) {
+                            return teamService.getTeamData("miamiheat");
+                        }
+                    }
+                }
+            }
+        })
+        .state("teams", {
+            url: '/teams/:team',
+            templateUrl: "js/teams/teamTmpl.html",
+            controller: "teamCtrl",
+            resolve:{
+                teamData: function(teamService, $stateParams) {
+                    return teamService.getTeamData($stateParams.team);
+            }
+        }
+        })
+
+        $urlRouterProvider.otherwise('/');
 
 });
--- nbaRoutes/js/teams/teamService.js
+++ nbaRoutes/js/teams/teamService.js
@@ -1,7 +1,50 @@
 var app = angular.module('nbaRoutes');
 
 app.service('teamService', function ($http, $q) {
+//make a method addNewGame This method is going to take in a gameObject// as the
+//parameter That gameObj will eventually have data about each individual game that we'll send to parse.
 
-    // service code
+    this.addNewGame = function(gameObj) {
+        var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+        if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+            gameObj.won = true;
+        } else {
+            gameObj.won = false;
+        }
+//Under your if statement, we're going make a POST request to parse to our URL we made earlier,
+// sending gameObj as the data. So, return the result of making an $http request with the 'method'
+//of 'POST', the 'url' being the URL variable we made earlier, and 'data' being our gameObj.
+        return $http({
+            method: "POST",
+            url: url,
+            data: gameObj
+        })
+    }
+
+        this.getTeamData = function(team) {
+            var deferred = $q.defer();
+            var url = 'https://api.parse.com/1/classes/' + team;
+            $http({
+                method: "GET",
+                url: url
+            })
+            .then(function(data) {
+                var results = data.data.results;
+                var wins = 0;
+                var losses = 0;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].won === true) {
+                        wins++;
+                    } else {
+                        losses++;
+                    }
+                }
+                results.wins = wins;
+                results.losses = losses;
+                deferred.resolve(results)
+                console.log(results)
+            })
+            return deferred.promise;
+        }
 
 });
